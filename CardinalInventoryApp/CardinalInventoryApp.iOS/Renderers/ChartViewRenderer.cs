119d1566d7522aa3db9fa76784133b652fa3c382
ryanrauch <rauch.ryan@gmail.com>
2018-07-22 00:33:20 -0500

    switching to pc

---
+++ CardinalInventoryApp/CardinalInventoryApp.iOS/Renderers/ChartViewRenderer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using CardinalInventoryApp.iOS.Renderers;
+using CardinalInventoryApp.Views.ContentPages;
+using Xamarin.Forms;
+
+[assembly: ExportRenderer(typeof(ChartViewBase), typeof(ChartViewRenderer))]
+
+namespace CardinalInventoryApp.iOS.Renderers
+{
+    public class ChartViewRenderer
+    {
+        public ChartViewRenderer()
+        {
+        }
+    }
+}
