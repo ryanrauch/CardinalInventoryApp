2e15158f42e6d629de2bb88c6f0026106942c64d
Ryan Rauch <rauch.ryan@gmail.com>
2018-08-10 13:18:32 -0500

    uwp - stub watchsession functions

---
+++ CardinalInventoryApp/CardinalInventoryApp.UWP/DependencyServices/StubWatchSessionManager.cs
@@ -0,0 +1,47 @@
+﻿using CardinalInventoryApp.Services.Interfaces;
+using CardinalInventoryApp.UWP.DependencyServices;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+[assembly: Dependency(typeof(StubWatchSessionManager))]
+namespace CardinalInventoryApp.UWP.DependencyServices
+{
+    public class StubWatchSessionManager : IWatchSessionManager
+    {
+        public event EventHandler<WatchDataEventArgs> DataReceived;
+
+        public bool IsPairedSession()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool IsReachableSession()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SendData(WatchDataType type, string data)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SendData(WatchDataType type, double x, double y, double z)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void StartSession()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void StopSession()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
